05336e91c7a81fe708af197cfd22cd9290715e9c
utility4u <benedikthofirek@gmail.com>
2019-04-17 14:10:57 +0200

    Updates

---
+++ precache-manifest.68b4bae05a2c4a048d3512d2ab3b9b2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed57e4d006c663f9b4b3",
+    "url": "/caterpillar/static/js/runtime~main.4ea3ef59.js"
+  },
+  {
+    "revision": "91ea1e527725467af1a9",
+    "url": "/caterpillar/static/js/main.64388308.chunk.js"
+  },
+  {
+    "revision": "cf66d56acceffc8b0d93",
+    "url": "/caterpillar/static/js/2.aee131eb.chunk.js"
+  },
+  {
+    "revision": "91ea1e527725467af1a9",
+    "url": "/caterpillar/static/css/main.3e01ffef.chunk.css"
+  },
+  {
+    "revision": "cf66d56acceffc8b0d93",
+    "url": "/caterpillar/static/css/2.1be28eca.chunk.css"
+  },
+  {
+    "revision": "0e9127087bc6baad9f58bf26ca05f4c6",
+    "url": "/caterpillar/index.html"
+  }
+];
